769331a6ed900f2375dda30041c705c43afbb521
Nii-101 <noelamptey101@gmail.com>
2023-06-20 18:00:08 -0700

    Task 5 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *
+ * @n: the number being worked on
+ *
+ *_putchar - prints a single character
+ *
+ *Return: 1 if greater than zero, 0 if = to zero and -1 if less than zero.
+ *
+*/
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}

68ae1ef8d0357e7a0ecddbca6a473cb05507071d
Nii-101 <noelamptey101@gmail.com>
2023-06-22 08:47:45 -0700

    Task 3 completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers- prints out numbers from 0 to 9
+ *
+ *
+ * Return: returns 0
+ *
+*/
+void print_numbers(void);
+{
+	char c;
+
+	for (c >= 48 && c < 58)
+	{
+		_putchar(c);
+	}
+	putchar("\n")
+}

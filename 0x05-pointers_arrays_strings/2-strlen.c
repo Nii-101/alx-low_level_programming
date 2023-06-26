36c0c97c4e02d8de6e1cb6a4c1a61b2e70e05936
Nii-101 <noelamptey101@gmail.com>
2023-06-26 15:44:43 -0700

    Commit to test if task 2 has been completed successfully

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strlen- returns the length of a string
+ *
+ *@s: the string being checked
+ *
+ *Return: length
+ *
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return (length);
+
+}

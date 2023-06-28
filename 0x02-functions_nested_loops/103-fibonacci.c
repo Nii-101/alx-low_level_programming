5b32e55f99dc228ab2483c55d41ca1c257f117c3
Nii-101 <noelamptey101@gmail.com>
2023-06-28 11:48:34 -0700

    Better late than never ... completed task 103 fibonacci sequence

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ *main- prints out sum of even numbers of the fibonacci sequence < 4000000
+ *
+ *@x: First value of the fibonacci sequence
+ *
+ *@y: second value of the fibonacci sequence
+ *
+ *@z: sum of the current and previous number of the sequence
+ *
+ *@a: sum of the current and previous even  number of the fibonacci sequence
+ *
+ *@b: sum of the even numbers of the fibonacci sequence, below 4000000
+ *
+ *Return: 0 means success
+*/
+int main(int x, int y, int z, int a, int b)
+{
+
+	x = 1, y = 2;
+
+	z = x + y;
+
+	a;
+
+	b;
+
+	for (z; z < 4000000; z = x + y)
+	{
+		x = y;
+		y = z;
+		{
+			if (y == z)
+			{	a = z + y;
+			}
+		}
+	}
+	b = a + 2;
+	printf("%d\n", b);
+
+	return (0);
+}

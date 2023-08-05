7e6f3c9ddb2b0a1e42118c7e6df6f06975b073b9
Nii-101 <noelamptey101@gmail.com>
2023-08-05 08:13:01 +0000

    Cleared trailing whitespace in main.h file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 /**
- * A main.h file containing the prototypes for the project
-*/ 
+ *MAIN_H- A header file containing the prototypes for the project
+*/
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
